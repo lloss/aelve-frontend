7675a65b4cb4fc5709aa31ba4cdbdbe8af69f632
Igor <ihorkotliar@gmail.com>
2019-01-19 17:11:56 +0200

    stores now implements their own types

--- src/entities/stores/HaskellRepoSearhFormStore.ts
+++ src/entities/stores/HaskellRepoSearhFormStore.ts
@@ -1,7 +1,9 @@
 import { injectable } from 'inversify'
 import { observable } from 'mobx';
 
+import { IHaskellRepoSearchFormStore } from '../../interfaces'
+
 @injectable()
-export class HaskellRepoSearhFormStore {
+export class HaskellRepoSearhFormStore implements IHaskellRepoSearchFormStore{
     @observable public haskell: string = "Haskell";
 }
